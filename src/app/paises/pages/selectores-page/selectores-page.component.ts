a0b9c21d4decea8a8a2cf4f63b4d859ef12566ca
angelxescomx08 <ah@xdevelop.mx>
2022-05-07 09:42:43 -0500

    router

---
+++ src/app/paises/pages/selectores-page/selectores-page.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-selectores-page',
+  templateUrl: './selectores-page.component.html',
+  styles: [
+  ]
+})
+export class SelectoresPageComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
